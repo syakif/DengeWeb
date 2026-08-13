53cbee8d62c48f38262d3f770fb069d712b4be36
Akif <snyakif@gmail.com>
2026-08-13 13:44:09 +0300

    key-value feature for "About" page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,20 +3,38 @@ using Microsoft.AspNetCore.Mvc;
 using DengeWeb.Models;
 using DengeWeb.Services;
 
+using Microsoft.Extensions.Configuration;
+
 namespace DengeWeb.Controllers;
 
 public class HomeController : Controller
 {
     private readonly IMailService _mailService;
+    private readonly IConfiguration _config;
 
-    public HomeController(IMailService mailService)
+    public HomeController(IMailService mailService, IConfiguration config)
     {
         _mailService = mailService;
+        _config = config;
     }
 
     public IActionResult Index() => View();
     
-    public IActionResult About() => View();
+    // About metodu artık dinamik veri yolluyor
+    public IActionResult About()
+    {
+        var model = new AboutViewModel
+        {
+            HeaderTitle = _config["AboutPageContent:HeaderTitle"],
+            HeaderSubtitle = _config["AboutPageContent:HeaderSubtitle"],
+            MissionTitle = _config["AboutPageContent:MissionTitle"],
+            MissionText = _config["AboutPageContent:MissionText"],
+            MissionImage = _config["AboutPageContent:MissionImage"]
+        };
+
+        // Modeli sayfaya (View) gönderiyoruz
+        return View(model); 
+    }
 
     public IActionResult Products()
     {
---
+++ Models/AboutViewModel.cs
@@ -0,0 +1,10 @@
+namespace DengeWeb.Models;
+
+public class AboutViewModel
+{
+    public string HeaderTitle { get; set; }
+    public string HeaderSubtitle { get; set; }
+    public string MissionTitle { get; set; }
+    public string MissionText { get; set; }
+    public string MissionImage { get; set; }
+}
