b2f3ccd24f5d1cd05efed54e292695e98679ee41
Akif <snyakif@gmail.com>
2026-08-17 16:28:06 +0300

    about us index page section has been added

--- obj/Debug/net10.0/DengeWeb.AssemblyInfo.cs
+++ obj/Debug/net10.0/DengeWeb.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DengeWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4e0fdba787e1377bad30b616252128f296cef4c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e877a986a020dbb2178c6332fd0b1c2bf1de18d6")]
 [assembly: System.Reflection.AssemblyProductAttribute("DengeWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DengeWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
